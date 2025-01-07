b6d825217d5b531abeea074b45c5d6cc0505c170
mk <infisparks@gmail.com>
2025-01-07 18:02:02 -0500

    sdf

--- src/app/page.js
+++ src/app/page.js
@@ -29,6 +29,7 @@ export default function Home() {
       <Services />
       <Testimonial />
       {/* <Departments /> */}
+      
       <Pricing />
       {/* <Team /> */}
       {/* <Blog /> */}
